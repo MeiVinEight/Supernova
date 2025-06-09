eec60b46983971a79aad305fa39b6d8e82535eb6
MeiVinEight <MeiVinEight@outlook.com>
2025-06-09 16:34:20 +0800

    Sweeping Edge max level 5

---
+++ src/main/java/org/mve/sn/mixin/SweepingEdgeEnchantmentMixin.java
@@ -0,0 +1,17 @@
+package org.mve.sn.mixin;
+
+import net.minecraft.world.item.enchantment.SweepingEdgeEnchantment;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(SweepingEdgeEnchantment.class)
+public class SweepingEdgeEnchantmentMixin
+{
+	@Inject(at = @At("RETURN"), method = "getMaxLevel()I", cancellable = true)
+	public void getMaxLevel(CallbackInfoReturnable<Integer> cir)
+	{
+		cir.setReturnValue(5);
+	}
+}
