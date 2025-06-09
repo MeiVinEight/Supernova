e0b02a84e67cac22e53c4a81cf59dd59c1ed12eb
MeiVinEight <MeiVinEight@outlook.com>
2025-06-09 16:19:56 +0800

    Loyalty max level 5

---
+++ src/main/java/org/mve/sn/mixin/TridentLoyaltyEnchantmentMixin.java
@@ -0,0 +1,17 @@
+package org.mve.sn.mixin;
+
+import net.minecraft.world.item.enchantment.TridentLoyaltyEnchantment;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(TridentLoyaltyEnchantment.class)
+public class TridentLoyaltyEnchantmentMixin
+{
+	@Inject(at = @At("RETURN"), method = "getMaxLevel()I", cancellable = true)
+	public void getMaxLevel(CallbackInfoReturnable<Integer> cir)
+	{
+		cir.setReturnValue(5);
+	}
+}
