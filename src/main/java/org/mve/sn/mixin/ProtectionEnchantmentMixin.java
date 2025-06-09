b1565263957cf6f59ca55866f1b9a5c39f66cf33
MeiVinEight <MeiVinEight@outlook.com>
2025-06-09 16:32:51 +0800

    Any protection max level 5

---
+++ src/main/java/org/mve/sn/mixin/ProtectionEnchantmentMixin.java
@@ -0,0 +1,17 @@
+package org.mve.sn.mixin;
+
+import net.minecraft.world.item.enchantment.ProtectionEnchantment;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(ProtectionEnchantment.class)
+public class ProtectionEnchantmentMixin
+{
+	@Inject(at = @At("RETURN"), method = "getMaxLevel()I", cancellable = true)
+	public void getMaxLevel(CallbackInfoReturnable<Integer> cir)
+	{
+		cir.setReturnValue(5);
+	}
+}
