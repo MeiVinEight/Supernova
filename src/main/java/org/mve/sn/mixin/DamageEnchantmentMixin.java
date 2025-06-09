d8e8b935bc3201a11e06b70dd5b35c95cff75d10
MeiVinEight <MeiVinEight@outlook.com>
2025-06-09 16:15:20 +0800

    Sharpness max level 10

---
+++ src/main/java/org/mve/sn/mixin/DamageEnchantmentMixin.java
@@ -0,0 +1,19 @@
+package org.mve.sn.mixin;
+
+import net.minecraft.world.item.enchantment.DamageEnchantment;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(DamageEnchantment.class)
+public class DamageEnchantmentMixin
+{
+	@Inject(at = @At("RETURN"), method = "getMaxLevel()I", cancellable = true)
+	public void getMaxLevel(CallbackInfoReturnable<Integer> cir)
+	{
+		DamageEnchantment _this = (DamageEnchantment)(Object) this;
+		// Sharpness
+		if (_this.type == 0) cir.setReturnValue(10);
+	}
+}
