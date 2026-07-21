2b2ce7ee4477aa007a837681f853347a0c7f29db
MeiVinEight <MeiVinEight@outlook.com>
2026-07-21 17:26:31 +0800

    Remove some config

--- src/main/java/org/mve/sn/Configuration.java
+++ src/main/java/org/mve/sn/Configuration.java
@@ -7,28 +7,10 @@ import java.util.List;
 public class Configuration
 {
 	public static final ForgeConfigSpec.BooleanValue REPAIR_COST;
-	public static final ForgeConfigSpec.BooleanValue ENCHANTMENT_COMPATIBILITY;
-	public static final ForgeConfigSpec.IntValue MAX_LEVEL_BLASTPROTECTION;
-	public static final ForgeConfigSpec.IntValue MAX_LEVEL_FEATHERFALLING;
-	public static final ForgeConfigSpec.IntValue MAX_LEVEL_FIREPROTECTION;
-	public static final ForgeConfigSpec.IntValue MAX_LEVEL_FORTUNE;
-	public static final ForgeConfigSpec.IntValue MAX_LEVEL_LOOTING;
-	public static final ForgeConfigSpec.IntValue MAX_LEVEL_LOYALTY;
-	public static final ForgeConfigSpec.IntValue MAX_LEVEL_LUCKOFTHESEA;
-	public static final ForgeConfigSpec.IntValue MAX_LEVEL_POWER;
-	public static final ForgeConfigSpec.IntValue MAX_LEVEL_PROJECTILEPROTECTION;
-	public static final ForgeConfigSpec.IntValue MAX_LEVEL_PROTECTION;
-	public static final ForgeConfigSpec.IntValue MAX_LEVEL_SHARPNESS;
-	public static final ForgeConfigSpec.IntValue MAX_LEVEL_SWEEPING;
-	public static final ForgeConfigSpec.IntValue MAX_LEVEL_RIPTIDE;
-	public static final ForgeConfigSpec.IntValue MAX_LEVEL_UNBREAKING;
 	public static final ForgeConfigSpec.BooleanValue ENDER_BOW;
 	public static final ForgeConfigSpec.DoubleValue ENDER_SKELETON_PROBABILITY;
 	public static final ForgeConfigSpec.ConfigValue<List<? extends String>> ENTITY_EXPLOSION;
 	public static final ForgeConfigSpec.BooleanValue DIGGER_PICKUP;
-	public static final ForgeConfigSpec.BooleanValue KILLER_PICKUP;
-	public static final ForgeConfigSpec.BooleanValue SUPER_JUMP;
-	public static final ForgeConfigSpec.DoubleValue SUPER_JUMP_SCALE;
 	public static final ForgeConfigSpec.BooleanValue INFINITY_FIX;
 	public static final ForgeConfigSpec.BooleanValue ESCAPE_ALLOWED;
 	public static final ForgeConfigSpec SPECIFICATION;
@@ -39,51 +21,6 @@ public class Configuration
 		REPAIR_COST = builder
 			.comment("Whether or not enable repair cost")
 			.define("REPAIR_COST", true);
-		ENCHANTMENT_COMPATIBILITY = builder
-			.comment("Whether or not widen enchantment compatibility.")
-			.define("ENCHANTMENT_COMPATIBILITY", false);
-		MAX_LEVEL_BLASTPROTECTION = builder
-			.comment("Max level of blast protection")
-			.defineInRange("MAX_LEVEL_BLASTPROTECTION", 4, 1, 255);
-		MAX_LEVEL_FEATHERFALLING = builder
-			.comment("Max level of feather falling")
-			.defineInRange("MAX_LEVEL_FEATHERFALLING", 4, 1, 255);
-		MAX_LEVEL_FIREPROTECTION = builder
-			.comment("Max level of fire protection")
-			.defineInRange("MAX_LEVEL_FIREPROTECTION", 4, 1, 255);
-		MAX_LEVEL_FORTUNE = builder
-			.comment("Max level of fortune")
-			.defineInRange("MAX_LEVEL_FORTUNE", 3, 1, 255);
-		MAX_LEVEL_LOOTING = builder
-			.comment("Max level of looting")
-			.defineInRange("MAX_LEVEL_LOOTING", 3, 1, 255);
-		MAX_LEVEL_LOYALTY = builder
-			.comment("Max level of loyalty")
-			.defineInRange("MAX_LEVEL_LOYALTY", 3, 1, 255);
-		MAX_LEVEL_LUCKOFTHESEA = builder
-			.comment("Max level of luck of the sea")
-			.defineInRange("MAX_LEVEL_LUCKOFTHESEA", 3, 1, 255);
-		MAX_LEVEL_POWER = builder
-			.comment("Max level of power")
-			.defineInRange("MAX_LEVEL_POWER", 5, 1, 255);
-		MAX_LEVEL_PROJECTILEPROTECTION = builder
-			.comment("Max level of projectile protection")
-			.defineInRange("MAX_LEVEL_PROJECTILEPROTECTION", 4, 1, 255);
-		MAX_LEVEL_PROTECTION = builder
-			.comment("Max level of protection")
-			.defineInRange("MAX_LEVEL_PROTECTION", 4, 1, 255);
-		MAX_LEVEL_SHARPNESS = builder
-			.comment("Max level of sharpness")
-			.defineInRange("MAX_LEVEL_SHARPNESS", 5, 1, 255);
-		MAX_LEVEL_SWEEPING = builder
-			.comment("Max level of sweeping")
-			.defineInRange("MAX_LEVEL_SWEEPING", 3, 1, 255);
-		MAX_LEVEL_RIPTIDE = builder
-			.comment("Max level of trident")
-			.defineInRange("MAX_LEVEL_TRIDENT", 3, 1, 255);
-		MAX_LEVEL_UNBREAKING = builder
-			.comment("Max level of unbreaking")
-			.defineInRange("MAX_LEVEL_UNBREAKING", 3, 1, 255);
 		ENDER_BOW = builder
 			.comment("Whether or not enable ender bow")
 			.define("ENDERBOW", false);
@@ -96,15 +33,6 @@ public class Configuration
 		DIGGER_PICKUP = builder
 			.comment("Auto pickup item after destroy block when holding shift")
 			.define("DIGGER_PICKUP", false);
-		KILLER_PICKUP = builder
-			.comment("Auto pickup entity loots after kill entity")
-			.define("KILLER_PICKUP", false);
-		SUPER_JUMP = builder
-			.comment("Jump higher when holding shift")
-			.define("SUPER_JUMP", false);
-		SUPER_JUMP_SCALE = builder
-			.comment("Y speed scale for super jump")
-			.defineInRange("SUPER_JUMP_SCALE", 1.5, 1, Float.MAX_VALUE);
 		INFINITY_FIX = builder
 			.comment("Removes need to have an arrow in your inventory to use the Infinity enchant on your bow.")
 			.define("INFINITY_FIX", false);
