ba0f3ef795303ff5950116dffed51f7722202d69
MeiVinEight <MeiVinEight@outlook.com>
2026-07-27 21:20:35 +0800

    NBT auto wrap

--- src/client/java/org/mve/sn/client/mixin/ItemStackMixin.java
+++ src/client/java/org/mve/sn/client/mixin/ItemStackMixin.java
@@ -1,9 +1,13 @@
 package org.mve.sn.client.mixin;
 
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.nbt.NbtUtils;
 import net.minecraft.nbt.Tag;
 import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.FormattedText;
+import net.minecraft.network.chat.MutableComponent;
+import net.minecraft.network.chat.Style;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
@@ -14,6 +18,7 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.util.List;
+import java.util.Optional;
 
 @Mixin(ItemStack.class)
 public class ItemStackMixin
@@ -29,6 +34,21 @@ public class ItemStackMixin
 		ItemStack item = (ItemStack) (Object) this;
 		Tag tag = item.save(player.level().registryAccess());
 		Component component = NbtUtils.toPrettyComponent(tag);
-		cir.getReturnValue().add(component);
+		Minecraft instance = Minecraft.getInstance();
+		int width = instance.getWindow().getGuiScaledWidth();
+		width -= (int) instance.getWindow().getGuiScale() * 4;
+		List<FormattedText> list = instance.font.getSplitter().splitLines(component, width, Style.EMPTY);
+		for (FormattedText formattedText : list)
+		{
+			MutableComponent mutable = Component.empty();
+			formattedText.visit((a, b) ->
+			{
+				MutableComponent block = Component.literal(b);
+				if (!a.isEmpty()) block.withStyle(a);
+				mutable.append(block);
+				return Optional.empty();
+			}, Style.EMPTY);
+			cir.getReturnValue().add(mutable);
+		}
 	}
 }
